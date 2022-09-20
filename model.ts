016e868e855e3f3cdf6a4f4bb8dd2af5a9c41e29
m4r6i0 <m4r6i0@gmail.com>
2022-09-20 10:56:03 -0300

    faltou arquivo

---
+++ model.ts
@@ -0,0 +1,20 @@
+export class Sequence { 
+    
+    public rankId: number;
+    public n1: number;
+    public n2: number;
+    public n3: number;
+    public n4: number;
+    public n5: number;
+    public n6: number;
+
+    constructor(rankId: number, s1: number, s2: number, s3: number, s4: number, s5: number, s6: number) {
+        this.rankId = rankId;
+        this.n1 = s1;
+        this.n2 = s2;
+        this.n3 = s3;
+        this.n4 = s4;
+        this.n5 = s5;
+        this.n6 = s6;
+    }
+}
